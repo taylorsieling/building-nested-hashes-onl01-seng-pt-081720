7d1459f47c0b4787fbbc7398984f44ea6df84340
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 14:54:17 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -14,7 +14,7 @@ def bonus
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }

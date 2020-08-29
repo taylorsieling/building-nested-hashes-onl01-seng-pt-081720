5f199467939b3252ddeecd8259c525cc156a5c38
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 14:57:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
 
 epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:montague][:heroine][:status] = "dead"  
+epic_tragedy[:capulet][:heroine][:status] = "dead"  
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
